18e50faaafd1a6f6563e9fe5c57097d66da8aa7f
nathan-hadley <nathan.hadley@limblecmms.com>
2023-12-10 08:29:11 -0800

    add landing page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BuilderModule } from '@builder.io/angular';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BuilderModule.forRoot('549b46eee68249fba45d93651985ab9b'),
+    RouterModule.forRoot([
+      {
+        path: '**',
+        component: LandingPageComponent
+      },
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
